e204155708791c0c8c7cdcd1e4b0a5b656a1d8ea
Atr0pos <piersbrady@gmail.com>
2018-05-15 20:14:56 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,26 +1,7 @@
 import socket
-import threading
+from threading import Thread
+import copy
 import json
-
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5656
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-sock.bind(UDP_IP, UDP_PORT)
-
-players = {}
-
-
-udp_server = UdpServer(sock)
-udp_server.start()
-
-while True:
-	for player in players:
-		sock.sendto(data, player)
-		pass
-	pass	
-
 
 class UdpServer(Thread):
 	"""docstring for UdpServer"""
@@ -32,11 +13,38 @@ class UdpServer(Thread):
 
 		self.socket = sock
 		
-	def run():
+	def run(self):
 		while True:
-			data, addr = socket.recvfrom(1024)
+			data, addr = self.socket.recvfrom(1024)
 
 			if not addr in players:
-				players[addr] = {"transform" : json.dumps(data)["transform"]}
+				players[addr] = {"transform" : json.loads(data)}
 				pass
 			pass
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5656
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind((UDP_IP, UDP_PORT))
+
+players = {}
+
+
+udp_server = UdpServer(sock)
+udp_server.start()
+
+while True:
+	data = {}
+	tempData = copy.deepcopy(players)
+	for player in tempData:
+		data[player[1]] = tempData[player]
+		pass
+
+	for player in tempData:
+		sock.sendto(json.dumps(data), player)
+		pass
+	pass	
+
+
