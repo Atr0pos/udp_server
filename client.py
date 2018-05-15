625d43bec1ec173e3b38c9f08ba6d7b5f2bea862
Atr0pos <piersbrady@gmail.com>
2018-05-15 20:15:17 +0100

    Create client.py

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+import json
+
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5656
+
+sock = socket.socket(	socket.AF_INET, 
+						socket.SOCK_DGRAM	)
+
+data = {"transform" : {"x" : 0, "y" : 0, "z" : 0}}
+sock.sendto(json.dumps(data), (	UDP_IP, UDP_PORT	))
+
+while True:
+	data, addr = sock.recvfrom(1024)
+	print data
+	pass
